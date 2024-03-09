c70966297ee3f9203b221a8f3ae0d9cd81166112
shrish-nitb <shrish108@gmail.com>
2024-03-09 18:40:48 +0530

    Fixes

--- index.js
+++ index.js
@@ -13,9 +13,14 @@ app.use(cors({
   credentials:true,
   optionSuccessStatus:200,
 }));
+app.use("/", (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(cookieParser());
 app.use(bodyParser.json());
-
 const PORT = process.env.PORT;
 
 const userRouter = require("./router/user");
