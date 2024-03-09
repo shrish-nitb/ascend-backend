157f9ed487238dafaf5dc8b41cb5be73adb74868
shrish-nitb <shrish108@gmail.com>
2024-03-09 21:46:33 +0530

    Fixes

--- router/user.js
+++ router/user.js
@@ -6,7 +6,7 @@ const User = require("../model/user");
 
 router.use("/", async (req, res, next) => {
   let token = req.header("Authorization");
-  console.log(token)
+  console.log(req.cookies, token)
   if (token && token.startsWith("Bearer ")) {
     token = token.slice(7);
   } else if (req.cookies && req.cookies.token) {
