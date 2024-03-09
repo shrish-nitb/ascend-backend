17a91daaaaacdbcaf5e724cae4ed796ae592aba8
shrish-nitb <shrish108@gmail.com>
2024-03-09 19:14:16 +0530

    Fixes

--- index.js
+++ index.js
@@ -8,23 +8,15 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-app.use(cors());
-// app.use("/", (req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
 app.use(cookieParser());
 app.use(bodyParser.json());
+
 const PORT = process.env.PORT;
 
 const userRouter = require("./router/user");
 const ordersRouter = require("./router/order");
 const plansRouter = require("./router/plan");
 const questionsRouter = require("./router/question");
-
-
 
 app.use("/user", userRouter);
 app.use("/orders", ordersRouter);
