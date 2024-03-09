14e895217b518a0893637c481078a7eeb44f7a92
shrish-nitb <shrish108@gmail.com>
2024-03-09 21:49:24 +0530

    Fixes

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-app.use(cors());
 app.use(cookieParser());
+app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT;
