bab1c81b9d6c9ad3408462ba778d213dc776624d
shrish-nitb <shrish108@gmail.com>
2024-03-26 17:51:21 +0530

    practice

--- router/question.js
+++ router/question.js
@@ -6,7 +6,7 @@ const { Question, Answer } = require("../model/question");
 router.get('/solve/:questionId/:markedValue', async (req, res) => {
   try {
       const { questionId, markedValue } = req.params;
-      const answerDoc = await Answer.findOne({ _id: questionId }).exec();
+      const answerDoc = await Answer.findOne({ _id: questionId.trim() }).exec();
       if (!answerDoc) {
           return res.json({ match: false });
       }
