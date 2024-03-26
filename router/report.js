63fdb720d6ae6e6f9d853acb4cdeb3ca6b8296e4
shrish-nitb <shrish108@gmail.com>
2024-03-26 15:34:03 +0530

    Rank

--- router/report.js
+++ router/report.js
@@ -238,6 +238,7 @@ async function getAnalytics(report) {
     const {name, picture} = await User.find({user: item.user}, "name picture -_id").exec();
     item.name = name;
     item.picture = picture;
+    console.log(item)
     return item;
   }))
   console.log(modTop)
