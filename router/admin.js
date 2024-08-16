d386b3a2c9e508eaa28a81b2cd5ec7436bb7bcf3
shrish-nitb <shrish108@gmail.com>
2024-08-16 20:28:57 +0530

    get user info

--- router/admin.js
+++ router/admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { usersAll } = require("../utils/database");
+const { usersAll, reportsAll } = require("../utils/database");
 
 const { firebaseTokenVerifier, userAuthLookup } = require("../utils/middleware")
 
@@ -16,6 +16,15 @@ router.get("/users", async(req, res) => {
     try{
         const userList  = await usersAll();
         res.status(200).json(userList);
+    } catch (error){
+        res.status(500).json({ message: error.message });
+    }
+})
+
+router.get("/user/:uid", async(req, res) => {
+    try{
+        const reportList  = await reportsAll(req.params.uid);
+        res.status(200).json(reportList);
     } catch (error){
         res.status(500).json({ message: error.message });
     }
